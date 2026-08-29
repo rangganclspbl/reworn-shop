ebd8edae2b1716e36821f564f53108b8bcda7f80
rangganclspbl <ranggarimbaku@gmail.com>
2026-08-29 19:21:31 +0700

    improve mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ export function Navbar() {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [isSearchClosing, setIsSearchClosing] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const openSearch = () => {
     setIsSearchOpen(true);
@@ -25,47 +26,70 @@ export function Navbar() {
     setSearchQuery(event.target.value);
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <h1>ATHLEX</h1>
-      </div>
+    <>
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <h1>ATHLEX</h1>
+        </div>
 
-      <div className="navbar-links">
-        <a href="/">Home</a>
-        <a href="/shop">Shop</a>
-        <a href="/about">About</a>
-      </div>
+        <div className="navbar-links">
+          <a href="/">Home</a>
+          <a href="/shop">Shop</a>
+          <a href="/about">About</a>
+        </div>
 
-      <div className="navbar-actions">
-        {isSearchOpen ? (
-          <>
-            <input
-              value={searchQuery}
-              onChange={handleSearchChange}
-              className={isSearchClosing ? "search-input closing" : "search-input"}
-            />
-            {searchQuery === "" ? (
-              <button className="close-search" onClick={closeSearch}>
-                <X size={21} strokeWidth={2.5} />
-              </button>
-            ) : (
+        <div className="navbar-actions">
+          {isSearchOpen ? (
+            <>
+              <input
+                value={searchQuery}
+                onChange={handleSearchChange}
+                className={
+                  isSearchClosing ? "search-input closing" : "search-input"
+                }
+              />
+              {searchQuery === "" ? (
+                <button className="close-search" onClick={closeSearch}>
+                  <X size={21} strokeWidth={2.5} />
+                </button>
+              ) : (
+                <Search size={21} strokeWidth={2.5} />
+              )}
+            </>
+          ) : (
+            <button className="search-btn" onClick={openSearch}>
               <Search size={21} strokeWidth={2.5} />
-            )}
-          </>
-        ) : (
-          <button className="search-btn" onClick={openSearch}>
-            <Search size={21} strokeWidth={2.5} />
+            </button>
+          )}
+          <button className="cart-btn">
+            <ShoppingBag size={21} strokeWidth={2.5} />
           </button>
-        )}
-        <button className="cart-btn">
-          <ShoppingBag size={21} strokeWidth={2.5} />
-        </button>
-        <button className="ham-btn">
-          <Menu size={21} strokeWidth={2.5} />
-        </button>
-      </div>
-    </nav>
+          <button className="ham-btn" onClick={toggleMenu}>
+            {isMenuOpen ? (
+              <>
+                <X />
+              </>
+            ) : (
+              <>
+                <Menu />
+              </>
+            )}
+          </button>
+        </div>
+      </nav>
+      {isMenuOpen && (
+        <div className="mobile-menu">
+          <a href="/">Home</a>
+          <a href="/shop">Shop</a>
+          <a href="/about">About</a>
+        </div>
+      )}
+    </>
   );
 }
 
